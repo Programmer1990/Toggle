d31e9e7757d31e332ae7dd32b4282bb37978c1c4
Narinder <programmer.narinder.singh@gmail.com>
2021-10-20 10:41:27 +1300

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './index.css';
+import Student from './Student';
+
+function App(){
+  const[status, setStatus]=React.useState(true)
+     
+    
+  
+    return(
+        <div className="App">
+          {
+            status? 
+            <h1>Hello World!</h1>: null
+          }
+          
+         {/* <button onClick={()=>setStatus(false)}>Hide</button>
+          <button onClick={()=>setStatus(true)}>Show</button>*/}
+          <button onClick={()=>setStatus(!status)}>Toggle</button>
+          
+          
+           </div>
+    );
+   
+}
+
+
+    
+     
+
+export default App;
